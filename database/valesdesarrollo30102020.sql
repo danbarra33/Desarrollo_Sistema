134dcf864986b257219fed17f560235dc8d274f8
xcarlosbarraza <55636558+xcarlosbarraza@users.noreply.github.com>
2020-10-30 21:12:19 -0700

    Se agrego tabla sucursales y FK de clientes a sucursales

---
+++ database/valesdesarrollo30102020.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Sucursales (
+ID_Sucursal INT NOT NULL AUTO_INCREMENT,
+Capital DECIMAL(10,2) NOT NULL,
+Nombre_Empresa VARCHAR(50) NOT NULL,
+Direccion VARCHAR(50) NOT NULL,
+PRIMARY KEY(ID_Sucursusersal));
+
+ALTER TABLE users ADD CONSTRAINT cliente_a_sucursal FOREIGN KEY(ID_Sucursal) REFERENCES Sucursales(ID_Sucursal);
