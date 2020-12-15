c8ea909bbc1a058ba5f5d5aa2fed9dff4286d3b1
eberchaidez <eberchaidez@gmail.com>
2020-12-14 20:01:39 -0700

    actualizar cliente , controlador de buro de credito

--- app/Http/Controllers/ClientesController.php
+++ app/Http/Controllers/ClientesController.php
@@ -31,74 +31,106 @@ class ClientesController extends Controller
     }
 
     public function actualizar(Request $request){
+
+        $retorno = null;
         if(!isset($request->nombre) || strlen($request->nombre) == 0){
-            Session::flash('message', 'Captura el nombre'); 
-            Session::flash('alert-type', 'warning');
-            return back()->withInput();
+            $retorno = (object) array(
+                "codigo" => 0,
+                "mensaje" => "Debe igresar el nombre del cliente",
+                "cliente" => null
+            );
         }
         if(strlen(trim($request->nombre)) > 45){
-            Session::flash('message', 'El nombre debe contener máximo 45 caracteres'); 
-            Session::flash('alert-type', 'warning');
-            return back()->withInput();
+            $retorno = (object) array(
+                "codigo" => 0,
+                "mensaje" => "el nombre debe ser menor que 45 caracteres",
+                "cliente" => null
+            );
         }
 
         if(!isset($request->telefono) || strlen($request->telefono) == 0){
-            Session::flash('message', 'Captura el telefono'); 
-            Session::flash('alert-type', 'warning');
-            return back()->withInput();
+            $retorno = (object) array(
+                "codigo" => 0,
+                "mensaje" => "ingrese el telefono",
+                "cliente" => null
+            );
         }
         if(strlen(trim($request->telefono)) > 10){
-            Session::flash('message', 'El telefono debe contener máximo 10 caracteres'); 
-            Session::flash('alert-type', 'warning');
-            return back()->withInput();
+            $retorno = (object) array(
+                "codigo" => 0,
+                "mensaje" => "el telefono debe ser menor de 10 numeros",
+                "cliente" => null
+            );
         }
         if(!isset($request->domicilio) || strlen($request->domicilio) == 0){
-            Session::flash('message', 'Captura el domicilio'); 
-            Session::flash('alert-type', 'warning');
-            return back()->withInput();
+            $retorno = (object) array(
+                "codigo" => 0,
+                "mensaje" => "ingrese el domicilio",
+                "cliente" => null
+            );
         }
         if(strlen(trim($request->domicilio)) > 50){
-            Session::flash('message', 'El domicilio debe contener máximo 50 caracteres'); 
-            Session::flash('alert-type', 'warning');
-            return back()->withInput();
+            $retorno = (object) array(
+                "codigo" => 0,
+                "mensaje" => "el domicilio debe ser menor de 50 caracteres",
+                "cliente" => null
+            );
         }
         if(!isset($request->folioINE) || strlen($request->folioINE) == 0){
-            Session::flash('message', 'Captura el Folio INE'); 
-            Session::flash('alert-type', 'warning');
-            return back()->withInput();
+            $retorno = (object) array(
+                "codigo" => 0,
+                "mensaje" => "ingrese la INE",
+                "cliente" => null
+            );
         }
         if(strlen(trim($request->folioINE)) > 13){
-            Session::flash('message', 'El Folio INE debe contener máximo 13 caracteres'); 
-            Session::flash('alert-type', 'warning');
-            return back()->withInput();
+            $retorno = (object) array(
+                "codigo" => 0,
+                "mensaje" => "la Ine debe ser menor de 13 caracteres",
+                "cliente" => null
+            );
         }
         if(!isset($request->Salario) || strlen($request->Salario) == 0){
-            Session::flash('message', 'Captura el Salario'); 
-            Session::flash('alert-type', 'warning');
-            return back()->withInput();
+            $retorno = (object) array(
+                "codigo" => 0,
+                "mensaje" => "ingrese el salario",
+                "cliente" => null
+            );
         }
         if(!isset($request->status) || strlen($request->status) == 0){
-            Session::flash('message', 'Captura el Estatus'); 
-            Session::flash('alert-type', 'warning');
-            return back()->withInput();
+            $retorno = (object) array(
+                "codigo" => 0,
+                "mensaje" => "ingrese el estatus",
+                "cliente" => null
+            );
         }
         if(strlen(trim($request->status)) > 13){
-            Session::flash('message', 'El Estatus debe contener máximo 1 caracter'); 
-            Session::flash('alert-type', 'warning');
-            return back()->withInput();
+            $retorno = (object) array(
+                "codigo" => 0,
+                "mensaje" => "el estatus debe ser menor de 13 caracteres",
+                "cliente" => null
+            );
         }
 
         if(!isset($request->id) || strlen($request->id) == 0){
-            Session::flash('message', 'Debe proporcionar el id del cliente'); 
-            Session::flash('alert-type', 'warning');
-            return back()->withInput();
+            $retorno = (object) array(
+                "codigo" => 0,
+                "mensaje" => "ingrese el id",
+                "cliente" => null
+            );
         }
+
 
         $cliente = Cliente::where('iD_Cliente', $request->id)->first();
         if(!isset($cliente)){
-            Session::flash('message', 'El cliente debe proporcionado no existe'); 
-            Session::flash('alert-type', 'warning');
-            return back()->withInput();
+            $retorno = (object) array(
+                "codigo" => 0,
+                "mensaje" => "El cliente no existe",
+                "cliente" => null
+            );
+        }
+        if(isset($retorno)){
+            return json_encode($retorno);
         }
 
         $cliente->nombre = trim($request->nombre);
@@ -109,9 +141,13 @@ class ClientesController extends Controller
         $cliente->salario_Mensual = (float) trim($request->Salario);
         $cliente->save();
 
-        Session::flash('message', 'Cliente actualizado correctamente.'); 
-        Session::flash('alert-type', 'info');
-        return redirect('/clientes');
+        return json_encode((object) array(
+            "codigo" => 1,
+            "mensaje" => "cliente actualizado correctamente.",
+            "cliente" => $cliente
+        ));
+
+       
     }
 
     public function nuevo(Request $request){
